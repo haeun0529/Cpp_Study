13a97941dd5edc2e5010346b7bf446f49c2b2943
haeun0529 <h991512@naver.com>
2026-03-12 12:14:26 +0900

    Ch02 : 1번

---
+++ Chapter02/Ex01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int sum = 0;
+    for(int i=1; i<=10; i++) {
+        for(int j=1; j<=10; j++) {
+            sum ++;
+            cout << sum << '\t';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
