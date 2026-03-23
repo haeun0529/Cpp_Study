39c195749607215a9c6ec77a6320bb788d33522b
haeun0529 <h991512@naver.com>
2026-03-23 21:48:28 +0900

    Ch05 : 7번

---
+++ Chapter05/Ex07.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class MyIntStack {
+    int p[10];
+    int tos;
+public:
+    MyIntStack() {
+        tos = 0;
+    }
+    bool push(int n) {
+        if(tos < 10) {
+            p[tos] = n;
+            tos++;
+            return true;
+        }
+        return false;
+    }
+    bool pop(int &n) {
+        if(tos > 0) {
+            tos--;
+            n = p[tos];
+            return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+
+    MyIntStack a;
+    for(int i=0; i<11; i++) {
+        if(a.push(i)) cout << i << ' ';
+        else cout << endl << i+1 << " 번째 stack full" << endl;
+    }
+    int n;
+    for(int i=0; i<11; i++) {
+        if(a.pop(n)) cout << n << ' ';
+        else cout << endl << i+1 << " 번째 stack empty";
+    }
+
+    return 0;
+}
