d9ab6e1478c1b0ff375627cec176d1af75898981
haeun0529 <h991512@naver.com>
2026-03-31 11:33:13 +0900

    Ch07 : 8번

---
+++ Chapter07/Ex08.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+    int radius;
+public:
+    Circle(int radius=0) { this->radius = radius; }
+    void show() { cout << "radius = " << radius << " 인 원" << endl; }
+
+    Circle operator ++ () {
+        this->radius++;
+        return *this;
+    }
+
+    Circle operator ++ (int x) {
+        Circle tmp = *this;
+        radius++;
+        return tmp;
+    }
+};
+
+int main() {
+
+    Circle a(5), b(4);
+    ++a;
+    b = a++;
+    a.show();
+    b.show();
+
+}
