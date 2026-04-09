f6325ee2c85b5240f7f398dca8c7a660c4c6f5bb
haeun0529 <h991512@naver.com>
2026-04-09 13:19:34 +0900

    Ch10 : 4번

---
+++ Chapter10/Ex04.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+bool search(T sn, T *x, int size) {
+    for(int i=0; i<size; i++) {
+        if(x[i] == sn) return true;
+    }
+    return false;
+}
+
+int main() {
+
+    int x[] = {1, 10, 100, 5, 4};
+    if(search(100, x, 5)) cout << "100이 배열 x에 포함되어 있다";
+    else cout << "100이 배열 x에 포함되어 있지 않다";
+
+    return 0;
+}
